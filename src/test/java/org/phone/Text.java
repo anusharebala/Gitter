4ba05471731b86bca9d42146132182a94f5bd2d0
anusharebala <anusharebala@gmail.com>
2024-09-14 20:57:40 +0530

    added

--- src/test/java/org/phone/Text.java
+++ src/test/java/org/phone/Text.java
@@ -10,5 +10,16 @@ public class Text {
 	public void appium() {
 		System.out.println("appium");
 	}
+	public void selenium1() {
+		System.out.println("selenium");
+	}
+	public void framework1() {
+		System.out.println("framework");
+	}
+	public void appium1() {
+		System.out.println("appium");
+	}
+	
+	
 
 }
