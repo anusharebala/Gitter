1ac95dddc261c2dd35e65401cf9f0cc14e89c755
anusharebala <anusharebala@gmail.com>
2024-09-14 20:48:37 +0530

    started

---
+++ src/test/java/org/phone/Text.java
@@ -0,0 +1,14 @@
+package org.phone;
+
+public class Text {
+	public void selenium() {
+		System.out.println("selenium");
+	}
+	public void framework() {
+		System.out.println("framework");
+	}
+	public void appium() {
+		System.out.println("appium");
+	}
+
+}
